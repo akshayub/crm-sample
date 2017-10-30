e28340054153dc4fce5bada4406c9ed9026344cf
Amith Kini <kini.amit.22@gmail.com>
2017-10-30 18:30:28 +0530

    Add XML endpoint

--- objects/views.py
+++ objects/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .fetch import xml
+
+
 # Create your views here.
+def leads(request, size):
+    data = xml(int(size))
+    return HttpResponse(data)
